37c58f5d322ab0b4d3c11855ae63078472c7b672
Brett Sanders <brettrsanders@gmail.com>
2019-04-24 19:47:46 -0400

    Setup Firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase';
+
+// Initialize Firebase
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAe2BVC_iZ4xlyFHtnyDDjrUl0rCMXQd2g",
+  authDomain: "chat-app-brett.firebaseapp.com",
+  databaseURL: "https://chat-app-brett.firebaseio.com",
+  projectId: "chat-app-brett",
+  storageBucket: "chat-app-brett.appspot.com",
+  messagingSenderId: "968634074728"
+};
+firebase.initializeApp(config);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Nav">
+        <div className="User">
+          <img
+            className="UserImage"
+            alt="whatever"
+            src="https://placekitten.com/64/64"
+          />
+          <div>
+            <div>Brett Sanders</div>
+            <div>
+              <button className="text-button">log out</button>
+            </div>
+          </div>
+        </div>
+        <nav className="ChannelNav">
+          <a href="/channel/awesome"># awesome</a>
+          <a className="active" href="/channel/general">
+            # general
+          </a>
+        </nav>
+      </div>
+      <div className="Channel">
+        <div className="ChannelMain">
+          <div className="ChannelInfo">
+            <div className="Topic">
+              Topic: <input className="TopicInput" value="Awesome stuff" />
+            </div>
+            <div className="ChannelName">#general</div>
+          </div>
+          <div className="Messages">
+            <div className="EndOfMessages">That's every message!</div>
+            <div>
+              <div className="Day">
+                <div className="DayLine" />
+                <div className="DayText">12/6/2018</div>
+                <div className="DayLine" />
+              </div>
+              <div className="Message with-avatar">
+                <div className="Avatar" />
+                <div className="Author">
+                  <div>
+                    <span className="UserName">Ryan Florence </span>
+                    <span className="TimeStamp">3:37 PM</span>
+                  </div>
+                  <div className="MessageContent">Alright, lets do this.</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="Message no-avatar">
+                <div className="MessageContent">works now?</div>
+              </div>
+            </div>
+          </div>
+          <div className="ChatInputBox">
+            <input className="ChatInput" placeholder="Message #general" />
+          </div>
+        </div>
+        <div className="Members">
+          <div>
+            <div className="Member">
+              <div className="MemberStatus offline" />
+              Ryan Florence
+            </div>
+            <div className="Member">
+              <div className="MemberStatus online" />
+              cleverbot
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
