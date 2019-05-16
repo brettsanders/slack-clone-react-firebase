9de391f989d85c7c75e847c128669d8a1e6d2269
Brett Sanders <brettrsanders@gmail.com>
2019-05-15 23:39:22 -0400

    Handle Errors w/ Message in App Login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,50 @@ import Channel from './Channel';
 import {firebase} from './firebase';
 
 function App() {
+  const user = useAuth();
+
+  return user ? (
+    <div className="App">
+      <Nav user={user} />
+      <Channel />
+    </div>
+  ) : (
+    <Login />
+  );
+}
+
+function Login() {
+  const [authError, setAuthError] = useState(null);
+
+  const handleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .catch(error => {
+        setAuthError(error);
+      });
+  };
+
+  return (
+    <div className="Login">
+      <h1>Chat!</h1>
+      <button onClick={handleSignIn}>Sign in with Google</button>
+      {authError && (
+        <div>
+          <p>Sorry, there was a problem</p>
+          <p>
+            <i>{authError.message}</i>
+          </p>
+          <p>Please try again</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function useAuth() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -12,48 +56,16 @@ function App() {
         setUser({
           displayName: user.displayName,
           photoUrl: user.photoURL,
-          uid: user.uid
+          uid: user.uid,
         });
       } else {
         setUser(null);
       }
-      console.log(user)
+      console.log(user);
     });
   }, []);
 
-  const handleSignIn = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage);
-        console.log(email);
-        console.log(credential);
-      });
-  };
-
-  return user ? (
-    <div className="App">
-      <Nav user={user} />
-      <Channel />
-    </div>
-  ) : (
-    <div className="Login">
-      <h1>Chat!</h1>
-      <button onClick={handleSignIn}>Sign in with Google</button>
-    </div>
-  );
+  return user;
 }
 
 export default App;
