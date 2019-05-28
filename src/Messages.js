c5f876e47115267f0fafd0cb0625de7012921c1e
Brett Sanders <brettrsanders@gmail.com>
2019-05-27 21:54:22 -0400

    Show Topic info & Scroll Messages

--- src/Messages.js
+++ src/Messages.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import useCollection from './useCollection';
 import useDocWithCache from './useDocWithCache';
 import formatDate from 'date-fns/format';
 import isSameDay from 'date-fns/is_same_day';
 
+function useChatScrollManager(ref) {
+  useEffect(() => {
+    const node = ref.current;
+    node.scrollTop = node.scrollHeight;
+  });
+}
+
 function Messages({channelId}) {
   const messages = useCollection(`channels/${channelId}/messages`, 'createdAt');
+  const scrollerRef = useRef();
+  useChatScrollManager(scrollerRef);
 
   return (
-    <div className="Messages">
+    <div ref={scrollerRef} className="Messages">
       <div className="EndOfMessages">That's every message!</div>
 
       {messages.map((message, index) => {
@@ -41,7 +50,7 @@ function FirstMessageFromUser({message, showDay}) {
         <div className="Day">
           <div className="DayLine" />
           <div className="DayText">
-            { new Date(message.createdAt.seconds * 1000).toLocaleDateString() }
+            {new Date(message.createdAt.seconds * 1000).toLocaleDateString()}
           </div>
           <div className="DayLine" />
         </div>
@@ -75,8 +84,8 @@ function shouldShowDay(previous, message) {
 
   const isNewDay = !isSameDay(
     previous.createdAt.seconds * 1000,
-    message.createdAt.seconds * 1000
-  )
+    message.createdAt.seconds * 1000,
+  );
 
   return isNewDay;
 }
@@ -93,8 +102,7 @@ function shouldShowAvatar(previous, message) {
   }
 
   const hasBeenAWhile =
-    message.createdAt.seconds -
-    previous.createdAt.seconds > 180
+    message.createdAt.seconds - previous.createdAt.seconds > 180;
 
   return hasBeenAWhile;
 }
